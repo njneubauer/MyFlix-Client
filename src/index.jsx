d6e726653355a662459d6ad3dc272d377c6535a1
njneubauer <njneubauer@gmail.com>
2021-12-26 13:40:39 -0600

    development enviornment fixed. hot reload fixed.

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { MainView } from './components/main-view/main-view.jsx';
+import { MainView } from './Components/main-view/main-view.jsx';
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
@@ -16,8 +16,6 @@ class MyFlixApplication extends React.Component {
 
 // Finds the root of your app
 const container = document.getElementsByClassName('app-container')[0];
-
-if (module.hot) { module.hot.accept(); }
 
 // Tells React to render your app in the root DOM element
 ReactDOM.render(React.createElement(MyFlixApplication), container);
