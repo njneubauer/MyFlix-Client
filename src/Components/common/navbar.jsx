f250b8e8e0f80f0bd12fa2c7bed2ba6dd61ee79e
njneubauer <njneubauer@gmail.com>
2022-01-14 14:13:04 -0600

    added navbar component to common folder

---
+++ src/Components/common/navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Navbar, Nav, Container} from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+
+export function NavBar(props){
+    console.log(props);
+    const renderNavButtons = ()=>{
+        return props.user === null ? (
+            <div></div>
+        ) : (<div>
+                <ul className='links'>
+                    <li><Nav.Link>Profile</Nav.Link></li>
+                    <li><Button className='btn-logout' onClick={()=>props.onLogout()}>Logout</Button></li>
+                </ul>
+               
+            </div>
+            
+        )
+    };
+
+    return (
+        <Navbar className='py-0' bg="dark" variant="dark" sticky="top">
+            <Container className='nav-container' fluid>
+                <Nav>
+                    <Navbar.Brand align="top">
+                            <Link to='/'><h1>Nickflix</h1></Link>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                </Nav>
+                <Nav>
+                    {renderNavButtons()}
+                </Nav>
+            </Container>
+        </Navbar>
+    )
+}
